0a3421aa5d1279891ad45131e5194a60eedac22e
snowypark <partsnowy@gmail.com>
2022-07-19 10:06:29 +0900

    no message

---
+++ src/j01_출력/HelloJava.java
@@ -0,0 +1,16 @@
+package j01_출력;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		System.out.println("----");
+		
+		System.out.print("이름: ");
+		System.out.println("----");
+		System.out.print("연락처: ");
+		System.out.println("-----");
+	}
+
+}
+//개발 폰트 적용하기
---
+++ src/j01_출력/StudentInfo.java
@@ -0,0 +1,24 @@
+package j01_출력;
+
+public class StudentInfo {
+	public static void main(String[] args) {
+		
+		int year;
+		year = 2;
+		
+		System.out.println("이름: 김준일");
+		System.out.print("학년: ");
+		System.out.println(year);
+		System.out.println();
+		
+		System.out.println("이름: 김준이");
+		System.out.print("학년: ");
+		System.out.println(year);
+		System.out.println();
+		
+		System.out.println("이름: 김준삼");
+		System.out.print("학년: ");
+		System.out.println(year);
+		
+}
+}
