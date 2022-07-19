c16a778054558a5e1506b36535a91dfe92c4ada9
snowypark <partsnowy@gmail.com>
2022-07-19 10:48:29 +0900

    추상

---
+++ src/j14_추상/Human.java
@@ -0,0 +1,15 @@
+package j14_추상;
+
+public class Human extends Animal{
+	
+	@Override
+	public void move() {
+		System.out.println("사람이 두발로 걷습니다.");
+	}
+	
+	
+	public void readBook() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
+
+}
